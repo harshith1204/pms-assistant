65db090cd2e53feb1ce1c5f6809d90946ea54cdf
Harshith <harshithsai24@gmail.com>
2025-10-03 15:15:28 +0530

    fixed the initialization

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Vite dev server
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
